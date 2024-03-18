30a8a02cd85bc93bc5a886e167780c0297bab2ae
yubo <342730241@qq.com>
2024-03-18 23:47:00 +0800

    写成说明文档

--- 扩增正文审核辅助工具-20240312/index.py
+++ 扩增正文审核辅助工具-20240312/index.py
@@ -34,7 +34,6 @@ def computes(c, string):
     out = c[0:2]
     if out in string:
         rag = 2
-    print(rag)
     if len(c) > 15:
         couA = c[rag:15]
     else:
@@ -67,56 +66,35 @@ def main(sh, arr):
         # if index == 0:
         #     index += 1
         #     continue
+        # 循环常量
         for st in BASIC_DATA:
             logging.info('查询当前循环的数据'+ st)
             if st in s:
                 i = s.index(st)
                 sub_str = s[int(i):i + 15]
-                # 第一种情况
+                # 第一种情况 带一 二 三的情况
                 if sub_str in n:  # 带BASIC_DATAg的情况
-                    print("查询到了111")
                     wz = n.index(sub_str)
                     ss = list(n)
                     ss.insert(wz, '\n')  # 在索引位置插入一个空格
                     n = ''.join(ss)
-                    print("-------ssss---")
-                    print(n)
-                    print("-------ssss---")
                 else:  # 不带一二三的情况
                     sub_str = s[int(i):i + 15]
-                    print("查询出2222222")
                     print(sub_str)
                     sp = sub_str.split("。")
                     sub_str2 = s[int(i) + 2: (i + 2 + len(sp[0]))]
-                    print("输出sub_str2sub_str2sub_str2")
-                    print(sub_str2)
-                    print("输出sub_str2sub_str2sub_str2")
-                    print(n)
-                    print(sub_str in n)
-                    print("第几次" + str(index))
-                    print(sub_str in "\n")
-                    # 第二种情况
+                    # 第二种情况左列存在一二三的情况
                     if sub_str in n:
                         wz = n.index(sub_str)
                         character = n[0:wz]
-                        print("输出：")
                         character_list = [s for s in character.split("\n") if s]
-                        print(character_list)
                         if len(character_list) != 1:
                             wz = wz-2
-                        print(wz)
-                        print("-------")
-                        print(n[0:wz])
-                        print("-------")
-                        print("数字")
                         ss = list(n)
                         ss.insert(wz, '\n' + st)  # 在索引位置插入一个空格
                         n = ''.join(ss)
-                        print("-------查询到了---")
-                        print(n)
-                        print("-------查询到了---")
                         continue
-                    # 第三种情况
+                    # 第三种情况不处理
                     elif sub_str2 in n:
                         logging.info("是什么" + n)
                         logging.error('第三种情况')
@@ -129,62 +107,49 @@ def main(sh, arr):
             else:
                 logging.error('不存在')
         nl = [s for s in n.split("\n") if s]
-        print("000000000000000000")
         print(nl)
         itemss["b"] = "\n".join(nl)
         index += 1
 
-    # 第四种情况
+    '''
+    第四种情况处理第一行和第二行分行情况
+    '''
     for i in arr:
         # 把字符串拆分成数组
         a = i['a'].split("\n")
         b = i['b'].split("\n")
         bs = i['b']
-        print("进入进入进入进入进入进入")
         if len(a) != len(b):
-            print("------------_________--------")
-            logging.info("BBBBB" + b[0])
-            logging.info("AAAAA" + a[1])
             if a[1] in b[0]:
                 wz = bs.index(a[1])
                 ss = list(bs)
                 ss.insert(wz, '\n')  # 在索引5的位置插入一个空格
                 bs = ''.join(ss)
-                print("1111111")
                 print(bs)
                 i['b'] = bs
             else:
-                print("进入进入进入进入进入进入2222222222222")
                 if a[0] in b[0]:
-                    print("走包含了111111")
-                    print(len(a[0]))
                     wz = bs.index(a[0])
                     ss = list(bs)
                     ss.insert(wz + len(a[0]), '\n')
                     bs = ''.join(ss)
-                    print(bs)
                     i['b'] = bs
-            print("输出BBBB")
-            print(i['b'].split("\n"))
             a = [s for s in i['b'].split("\n") if s]
             i['b'] = '\n'.join(a)
         else:
             if a[0] in b[0]:
-                print("AAAAAAAAA1111:" + a[1])
-                print("BBBBBBBBB1111:" + b[0])
-                print("CCCCCCCCC:" + str(len(a[0])))
                 ss = list(bs)
                 ss.insert(int(len(a[0])), '\n')  # 在索引5的位置插入一个空格
                 bs = ''.join(ss)
-                print(bs)
                 i['b'] = bs
-                print("8888888888888888888888888")
             else:
-                print("99999999999999999999999999999999999")
+                print("这种情况不处理")
             a = [s for s in i['b'].split("\n") if s]
             i['b'] = '\n'.join(a)
 
-    # 第五种情况有些带了一二三有些没带说明前面的字符串不配需拼接上一二三
+    '''
+    第五种情况有些带了一二三有些没带说明前面的字符串不配需拼接上一二三
+    '''
     for item in arr:
         a = item["a"].split("\n")
         b = item["b"].split("\n")
@@ -194,19 +159,13 @@ def main(sh, arr):
                 deep_copy_list = copy.deepcopy(aa)
                 cou_a = computes(deep_copy_list, BASIC_DATA)
                 cou_b = computes(b[i], BASIC_DATA)
-                print("查询条数")
-                logging.info("BBBBB" + cou_a)
-                logging.info("AAAAA" + cou_b)
-                print(jaccard_similarity(cou_a, cou_b))
                 if jaccard_similarity(cou_a, cou_b) > 0.70:
                     sindex = aa[0:2]
-                    print(sindex)
                     if sindex in BASIC_DATA:
                         if sindex in b[i]:
                             b[i] = b[i]
                         else:
                             b[i] = sindex + b[i]
-                        print("进；来了")
         item["b"] = "\n".join(b)
 
 
@@ -235,23 +194,16 @@ def output(arr, output_name):
         z = arr[item]
         a = z['a'].split("\n")
         b = z['b'].split("\n")
-        print(b)
         final_data = []
-        print("a循环匹配b 如果b")
         id = 0
         index_id = []
         for aStr in a:
             for i in range(0, len(b)):
-                print(jaccard_similarity(aStr, b[i]))
                 if jaccard_similarity(aStr, b[i]) > 0.85:
-                    print("进入记录成功匹配的")
-                    print("aaa:" + aStr)
-                    print("bbb:" + str(id) +"内部循环id"+ str(i)+ "值：" + b[i])
                     #必须要得到B列的key值
                     index_id.append(i)
                     break
             id += 1
-        print(index_id)
         # 组装数据并设置飘红
         for k in range(0, len(b)):
             if k in index_id:
